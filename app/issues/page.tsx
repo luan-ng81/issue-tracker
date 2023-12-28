aed2cdeb7c0aa92220c7c0d0c87d4c6e64ed49f7
luan-ng81 <luannguyen8118@gmail.com>
2023-12-28 14:29:34 +0700

    Disable SSR for loading md editor

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -3,12 +3,11 @@ import React from "react";
 import Link from "../components/Link";
 import prisma from "@/prisma/client";
 import IssueStatusBadge from "../components/IssueStatusBadge";
-import delay from "delay";
 import IssueActions from "./IssueActions";
 
 const IssuesPage = async () => {
   const issues = await prisma.issue.findMany();
-  await delay(2000);
+
   return (
     <div>
       <IssueActions />
